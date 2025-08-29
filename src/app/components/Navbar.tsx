394a4a5310ff8d26e6a7cee0e9d83a6a22f737cd
nyles <nrandall@sfsu.edu>
2025-08-28 20:42:41 -0700

    init

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <ul className="navbar">
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+        <li>Mission</li>
+      </ul>
+    </nav>
+  );
+}
