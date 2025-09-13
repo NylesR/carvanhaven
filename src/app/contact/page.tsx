f30abc4341f2b0af569e2098f1fbac338c9622ec
nyles <nrandall@sfsu.edu>
2025-09-13 10:10:25 -0700

    adds blank pages for sections

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,14 @@
+import Card from "../components/Card";
+import Button from "../components/Button";
+
+export default function Contact() {
+  return (
+    <>
+      <div className="bg-field min-h-screen">
+        <div className="bg-background/92 w-3/4 min-h-screen p-8 rounded mx-auto">
+        
+        </div>
+      </div>
+    </>
+  );
+}
